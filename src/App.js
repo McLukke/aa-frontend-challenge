7d01d4ab506ef16ab4f7310bdd352c9dfa3bd743
Kenny Luk <kenny@whynotkenny.com>
2022-03-14 20:46:46 -0400

    install sass & antd / setup state & render city links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Row, Col } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styles from "./App.module.scss";
+
+const cities = ["Ottawa", "Moscow", "Tokyo"];
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedCity: cities[0],
+    };
+  }
+
+  handleCitySelect = (selectedCity) => this.setState({ selectedCity });
+
+  render() {
+    const { selectedCity } = this.state;
+    const dudUrl = "#";
+
+    return (
+      <>
+        <Row justify="center" align="middle">
+          <Col xs={24} md={20} lg={16} xl={12} xxl={4}>
+            <div className={styles.cityLinks}>
+              {cities.map((city) => (
+                <a
+                  key={city}
+                  href={dudUrl}
+                  className={city === selectedCity ? styles.active : ""}
+                  onClick={() => this.handleCitySelect(city)}
+                >
+                  {city.toUpperCase()}
+                </a>
+              ))}
+            </div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={24} md={20} lg={16} xl={12} xxl={4}></Col>
+        </Row>
+      </>
+    );
+  }
 }
-
-export default App;
